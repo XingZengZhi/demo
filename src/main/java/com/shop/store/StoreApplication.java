cbf14d4fbb6a1dc7a4ec250b4ed82351b44bd6ad
XingZengZhi <944047118@qq.com>
2019-03-22 17:13:58 +0800

    去掉多余的包

--- src/main/java/com/shop/store/StoreApplication.java
+++ src/main/java/com/shop/store/StoreApplication.java
@@ -2,12 +2,11 @@ package com.shop.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class StoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
 	}
-
 }
